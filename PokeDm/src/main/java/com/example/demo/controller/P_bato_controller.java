ae1d0773ed6b324fb2872d62be6eab5dc98f033a
kuni29oft <kuni__>
2023-11-09 15:14:35 +0900

    home,計算,search,controllerを作成

---
+++ PokeDm/src/main/java/com/example/demo/controller/P_bato_controller.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.ui.Model;
+
+public class P_bato_controller {
+	
+	public String battle(Model model) {
+		return null;
+		
+	}
+
+}
