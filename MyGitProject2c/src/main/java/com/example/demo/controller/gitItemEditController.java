7d3b20936c8e6fea5aa214225111fcd2013d87e5
KA-TETO <kazuki.a.1613@gmail.com>
2023-11-17 14:26:35 +0900

    edit #2

--- MyGitProject2c/src/main/java/com/example/demo/controller/gitItemEditController.java
+++ MyGitProject2c/src/main/java/com/example/demo/controller/gitItemEditController.java
@@ -1,5 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class gitItemEditController {
+	@RequestMapping(path = "/xxx", method = RequestMethod.GET)
+	public String doEditGet() {
+		return "gitlogin";
+	}
+	@RequestMapping(path = "/xxxx", method = RequestMethod.POST)
+	public String doEditPost(String login,String pw) {
+		if(login.equals("gitlogin")&& pw.equals("gitpw")) {
+			return "gititemlist";
+		}else {
+		return "gitlogin";
+	}
+	}
 
 }
